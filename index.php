051897f876172d37fed7a7c4b6a00b6a5a71d605
Tejendra Gehlot <Tejendra Gehlot>
2016-01-09 16:18:44 +0530

    claim list design

---
+++ index.php
@@ -0,0 +1,42 @@
+<style>
+.mainDiv a {
+        font-family: Helvetica;
+        font-size:   1em;
+        font-weight: bold;
+        color      : #000099;
+        text-decoration: none;
+    }
+.mainDiv a:link    { color: #00ff00; }
+.mainDiv a:visited { color: #009900; }
+.mainDiv a:hover   { color: #66ff66; }
+.mainDiv a:active  { color: #ffff00; }
+
+.mainDiv a.greenButtonRound {
+    border: 2px solid #006600;
+    border-radius: 5px;
+    background-color: #009900;
+    color: #ffffff;
+    font-weight: bold;
+    text-decoration: none;
+    margin : 20px;
+    padding: 10px 20px 10px 20px;
+    display: inline-block;
+}
+.mainDiv a.greenButtonRound:hover {
+    border: 2px solid #009900;
+    background-color: #00cc00;
+}
+</style>
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<div class="mainDiv" align="center">
+<a class="greenButtonRound" href="flowers.php">Flowers Assignment</a>
+<a class="greenButtonRound" href="two_strings.php">Two Strings</a>
+<a class="greenButtonRound" href="ClaimList">Claim List Dropdown</a>
+</div>
+
+</body>
+</html>
